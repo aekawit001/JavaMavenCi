bb55d2d5f78bb0c2aa95fb9fe7a47d84cf9a2e59
aekawit001 <aakawit_13@hotmail.co.th>
2018-10-04 13:35:25 +0700

    commit

---
+++ src/test/java/com/test/ExampleTest.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		//init value
+		int score =80;
+	
+		// expected
+		String experted ="A";
+	
+		//actual
+		String actual ="A";
+		//test
+		Assert.assertEquals(experted, actual);
+		
+	}
+
+}
